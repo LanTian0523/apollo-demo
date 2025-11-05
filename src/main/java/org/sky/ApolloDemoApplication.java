986ecfc9f4c1aa3e091e39fec8bb733f219438f4
lantian <lt02415933@cainiao.com>
2025-11-05 15:08:13 +0800

    集成RocketMQ

--- src/main/java/org/sky/ApolloDemoApplication.java
+++ src/main/java/org/sky/ApolloDemoApplication.java
@@ -1,13 +1,25 @@
 package org.sky;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.sky.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableApolloConfig
-public class ApolloDemoApplication {
+public class ApolloDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private MessageProducer producer;
+
     public static void main(String[] args) {
         SpringApplication.run(ApolloDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        producer.send("Hello RocketMQ");
     }
 }
